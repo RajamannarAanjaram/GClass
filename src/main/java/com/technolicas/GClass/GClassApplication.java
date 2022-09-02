c28789d4517f2443003d68f1fffcee7814e7d5b2
RajamannarAanjaram <rajamannaraanjaram@gmail.com>
2022-09-02 14:33:47 +0530

    base commit

---
+++ src/main/java/com/technolicas/GClass/GClassApplication.java
@@ -0,0 +1,13 @@
+package com.technolicas.GClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technolicas/GClass/GClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technolicas.GClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
